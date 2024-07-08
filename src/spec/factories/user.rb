5f2dacd6c6491aef98d4502c63bca76ffc60192a
YutoFuji <iwasborninjan@gmail.com>
2024-07-08 10:56:28 +0900

    Delete commentout

--- src/spec/factories/user.rb
+++ src/spec/factories/user.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
     name { "testuser1" }
-    # email { "aaa@example.com" }
     sequence(:email) { |n| "tester#{n}@example.com" }
     password { "abcd1234" }
     register_status { "incomplete" }
