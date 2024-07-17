4f9db873b974e01e24bf0dfd568dc37f68325b11
YutoFuji <iwasborninjan@gmail.com>
2024-07-18 00:08:06 +0900

    登録時メールが届くように

--- src/app/mailers/password_reset_mailer.rb
+++ src/app/mailers/password_reset_mailer.rb
@@ -3,7 +3,7 @@ class PasswordResetMailer < ApplicationMailer
 
   def password_reset(user, address)
     @user = user
-    @url  = "http://localhost:3000/login?token?token=#{@user.password_reset_token}"
+    @url  = "#{ENV["BACKEND_BASE_URL"]}/login?token=#{@user.password_reset_token}"
     mail(to: address, subject: "パスワード変更を承りました")
   end
 end
--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -66,8 +66,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.delivery_method = :letter_opener_web
+  # config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 8888 }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["GMAIL_ADDRESS"],
+    password: ENV["GMAIL_APP_PASSWORD"]
+  }
 end
